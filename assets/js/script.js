7e1f596b2848647f738c3eb42e0392e7329efd82
EsterDahri <esterdahri@gmail.com>
2023-09-20 15:27:40 +0200

    try method without loop first

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,18 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        randomEmails: '',
       }
+    },
+
+    mounted(){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            console.log(response);
+
+           this.randomEmails = response.data.response
+            console.log(response.data.response);
+        })
     }
   }).mount('#app')
